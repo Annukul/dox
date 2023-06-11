1ee86f17a80adcaa814bbc0efa93418901f40521
Annukul Thakran <53966193+Annukul@users.noreply.github.com>
2023-06-11 20:53:27 +0530

    Radix UI added + auth page UI added

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { SignIn } from "../pages/Signin"
+import { Authentication } from "../pages/Authentication"
 import { Home } from "../pages/Home"
 
 export const Routers = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/auth" element={<Authentication />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
---
+++ src/ui/index.ts
@@ -0,0 +1,4 @@
+export { Input } from "./input"
+export { Button, buttonVariants } from "./button"
+export { Label } from "./label"
+export { Icons } from "./icons"
