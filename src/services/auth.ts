1ec464a471eb981e06cccd711c642dd58468d96c
Annukul Thakran <annukulthakran@Annukuls-MacBook-Air-2.local>
2023-07-30 15:24:56 +0530

    Base api env variable added

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,5 +4,5 @@ export const authorize = async (payload: { code: string }) => {
   try {
     const res = await axios.post(`${process.env.PUBLIC_API_BASE_URL}/github/callback`, payload)
     return res
-  } catch (error) {}
+  } catch (err) {}
 }
