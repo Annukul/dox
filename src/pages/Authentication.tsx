856b66b485c42e13a2a3693a34c5e7b18f3d5dd1
Annukul Thakran <annukulthakran@192.168.1.3>
2023-08-22 23:52:02 +0530

    Redirect url updated

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -22,7 +22,7 @@ export const Authentication = () => {
           href="/examples/authentication"
           className={cn(
             buttonVariants({ variant: "ghost", size: "sm" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
+            "absolute right-4 top-4 md:right-8 md:top-8 "
           )}
         >
           Login
